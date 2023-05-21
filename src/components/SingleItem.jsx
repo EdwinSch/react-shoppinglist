36f2765e45e614a2f1a16fdf4f101af69791ae3f
front-ed <edwinschutjes@gmail.com>
2023-05-21 14:16:05 +0200

    update: single item component with checl-off functionality

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,5 +1,29 @@
-const SingleItem = () => {
-  return <h2>single</h2>;
+import { useState } from "react";
+
+const SingleItem = ({ item, removeItem }) => {
+  const [isChecked, setIsChecked] = useState(item.completed);
+
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        id="done"
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <p
+        style={{
+          textDecoration: isChecked && "line-through",
+          textTransform: "capitalize",
+        }}
+      >
+        {item.name}
+      </p>
+      <button type="button" className="btn remove-btn">
+        delete
+      </button>
+    </div>
+  );
 };
 
 export default SingleItem;
